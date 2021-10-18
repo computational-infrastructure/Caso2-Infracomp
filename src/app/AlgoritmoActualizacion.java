7382acc86c4a083e23b74bebe41ed4ce29138609
Juan Alegría <juanszalegria@gmail.com>
2021-10-18 09:22:14 -0500

    Feat: update NRU algorithm

--- src/app/AlgoritmoActualizacion.java
+++ src/app/AlgoritmoActualizacion.java
@@ -5,25 +5,69 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class AlgoritmoActualizacion extends Thread {
 	private ConcurrentHashMap<Integer, Pagina> tabla;
-	private static ArrayList<Integer> pagesNRU;
+	// Class 0: not referenced, not modified.
+	ArrayList<Pagina> c0;
+	// Class 1: not referenced, modified.
+	ArrayList<Pagina> c1;
+	// Class 2: referenced, not modified.
+	ArrayList<Pagina> c2;
+	// Class 3: referenced, modified.
+	ArrayList<Pagina> c3;
 
-	public AlgoritmoActualizacion(ConcurrentHashMap<Integer, Pagina> tabla, Integer pageReferenced) {
+	public AlgoritmoActualizacion(ConcurrentHashMap<Integer, Pagina> tabla) {
 		this.tabla = tabla;
-		AlgoritmoActualizacion.pagesNRU.add(pageReferenced);
+	}
+
+	private void getBitPageCategories() {
+		for (Integer key : tabla.keySet()) {
+			Pagina page = tabla.get(key);
+			
+			// Not referenced
+		    if (!page.getR()) {
+		    	if (!page.getM()) {
+		    		c0.add(page);
+		    	} else {
+		    		c1.add(page);
+		    	}
+		    }
+		    // Referenced
+		    if (page.getR()) {
+		    	if (!page.getM()) {
+		    		c2.add(page);
+		    	} else {
+		    		c3.add(page);
+		    	}
+		    }
+		}
+	}
+	
+	private void removePageNRU() {
+		ArrayList<Pagina> selectedClass = null;
+		if (!c0.isEmpty()) {
+			selectedClass = c0;
+		}else if (c1.isEmpty()) {
+			selectedClass = c1;
+		}else if (c2.isEmpty()) {
+			selectedClass = c2;
+		}else if (c3.isEmpty()) {
+			selectedClass = c3;
+		}
+		
+		int index = (int) (Math.random() * selectedClass.size());
+		Integer pageNum = selectedClass.get(index).getNumPagina();
+		Pagina page = tabla.get(pageNum);
+		page.unload();
 	}
 
 	public void run() {
-		try {
-			Thread.sleep(20);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		int index = (int) (Math.random() * AlgoritmoActualizacion.pagesNRU.size());
-		// Random page from the group of not referenced in 20ms.
-		Integer pageNum = AlgoritmoActualizacion.pagesNRU.get(index);
-
-		Pagina page = tabla.get(pageNum);
-		page.resetR();
+	    try {
+	        while (true) {
+	            Thread.sleep(20);
+	    		getBitPageCategories();
+	            removePageNRU();
+	        }
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
 	}
 }
